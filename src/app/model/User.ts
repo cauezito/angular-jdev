1b64ffd042925e380ba2aa3d291a433d7a5f907c
Cauê Santos <caah.sfc@live.com>
2020-08-11 15:29:05 -0300

    Criando modelo e service para usuário

---
+++ src/app/model/User.ts
@@ -0,0 +1,6 @@
+export class User{
+    id: Number;
+    login: String;
+    name: String;
+    surname: String;
+}
---
+++ src/app/service/UserService.ts
@@ -0,0 +1,18 @@
+import {Injectable} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Constants } from '../app.constants';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class UserService{
+    constructor(private http: HttpClient){}
+    
+    getUsers() : Observable<any>{
+        return this.http.get<any>(Constants.url);
+    }
+
+}
