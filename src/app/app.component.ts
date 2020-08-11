c5ffe73b48ee52fcbd4d6c47cada07b279a95dbd
Cauê Santos <caah.sfc@live.com>
2020-08-11 14:39:55 -0300

    Criando menu de navegação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
-import {LoginService } from './service/LoginService';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Angular';
+export class AppComponent implements OnInit{
+  title = 'Usuários';
+
+  constructor(private router: Router){}
+
+  ngOnInit(): void {
+    if(localStorage.getItem('token') == null){
+      this.router.navigate(['login']);
+    } else {
+      this.router.navigate(['home']);
+    }
+  }
+
+  public logout(){
+    localStorage.clear();
+    this.router.navigate(['login']);
+  }
+
+
+ 
 
  
 
