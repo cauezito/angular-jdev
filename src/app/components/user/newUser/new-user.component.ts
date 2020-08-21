f34d82adeae31f4ec266b828aab2ee6cbf126aa2
Cauê Santos <caah.sfc@live.com>
2020-08-21 17:29:46 -0300

    Formatando a data recebida

--- src/app/components/user/newUser/new-user.component.ts
+++ src/app/components/user/newUser/new-user.component.ts
@@ -1,13 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/model/User';
 import { UserService } from 'src/app/service/UserService';
 import { Telephone } from 'src/app/model/telephone';
+import { NgbDateParserFormatter, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+
+@Injectable()
+export class FormatDate extends NgbDateParserFormatter{
+
+  readonly DELIMITER = '/';
+  parse(value: string): NgbDateStruct | null {
+    if(value){
+      let date = value.split(this.DELIMITER);
+      return {
+        day: parseInt(date[0], 10),
+        month: parseInt(date[1], 10),
+        year: parseInt(date[2], 10)
+      };
+    }
+    return null;
+  }
+  format(date: NgbDateStruct): string | null {
+    return date ? this.formatDayAndMonth(date.day) + this.DELIMITER + 
+    this.formatDayAndMonth(date.month) + this.DELIMITER + date.year : '';
+  }
+
+  formatDayAndMonth(value){
+    if(value.toString !== '' && parseInt(value) <= 9){
+      return '0'+value;
+    }
+    return value;
+  }
+  
+}
 
 @Component({
   selector: 'app-new-user',
   templateUrl: './new-user.component.html',
-  styleUrls: ['./new-user.component.css']
+  styleUrls: ['./new-user.component.css'],
+  providers: [{provide: NgbDateParserFormatter, useClass: FormatDate}]
 })
 export class NewUserComponent implements OnInit {
 
